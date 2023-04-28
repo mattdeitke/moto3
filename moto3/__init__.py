900d6a23e8eba7146fac6584b48203afbb4a680b
Matt Deitke <mdeitke23@gmail.com>
2023-04-28 21:55:43 +0000

    refactor for pickle

--- moto3/__init__.py
+++ moto3/__init__.py
@@ -14,41 +14,42 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+sqs_client = boto3.client("sqs")
+sqs_resource = boto3.resource("sqs")
 
-def _upload_batch(x) -> None:
-    sqs = boto3.client("sqs")
-    batch, queue_url = x
-    sqs.send_message_batch(QueueUrl=queue_url, Entries=batch)
+
+def upload_batch(args):
+    batch, queue_url = args
+    sqs_client.send_message_batch(QueueUrl=queue_url, Entries=batch)
 
 
 class QueueManager:
     def __init__(self, queue_name: str) -> None:
-        self.sqs = boto3.resource("sqs")
         self.queue_name = queue_name
+        self.queue_url = self._get_queue_url(queue_name)
 
+    def _get_queue_url(self, queue_name):
         try:
-            self.queue = self.sqs.get_queue_by_name(QueueName=self.queue_name)
-            logger.info(f"Queue '{self.queue_name}' found.")
-        except self.sqs.meta.client.exceptions.QueueDoesNotExist:
-            logger.warning(
-                f"Queue '{self.queue_name}' not found. Creating a new queue."
-            )
-            self.queue = self.sqs.create_queue(QueueName=self.queue_name)
-            logger.info(f"Queue '{self.queue_name}' created successfully.")
-
-        self.queue_url = self.queue.url
+            queue = sqs_resource.get_queue_by_name(QueueName=queue_name)
+            logger.info(f"Queue '{queue_name}' found.")
+        except sqs_resource.meta.client.exceptions.QueueDoesNotExist:
+            logger.warning(f"Queue '{queue_name}' not found. Creating a new queue.")
+            queue = sqs_resource.create_queue(QueueName=queue_name)
+            logger.info(f"Queue '{queue_name}' created successfully.")
+        return queue.url
 
     @property
     def size(self) -> int:
-        return self.queue.attributes["ApproximateNumberOfMessages"]
+        queue = sqs_resource.Queue(self.queue_url)
+        return int(queue.attributes["ApproximateNumberOfMessages"])
 
     def upload(self, messages: list) -> None:
         messages = [
             {
-                "Id": str(i),
-                "MessageBody": message
-                if isinstance(message, str)
-                else json.dumps(message),
+                "Id": f"{i}",
+                "MessageBody": (
+                    message if isinstance(message, str) else json.dumps(message)
+                ),
             }
             for i, message in enumerate(messages)
         ]
@@ -59,10 +60,11 @@ class QueueManager:
         ]
         # use tqdm and multiprocessing to upload the batches
         with Pool(multiprocessing.cpu_count()) as p:
-            list(tqdm(p.imap(_upload_batch, batches), total=len(batches)))
+            list(tqdm(p.imap(upload_batch, batches), total=len(batches)))
 
     def get_next(self, max_messages: int = 1) -> list:
-        response = self.queue.receive_messages(MaxNumberOfMessages=max_messages)
+        queue = sqs_resource.Queue(self.queue_url)
+        response = queue.receive_messages(MaxNumberOfMessages=max_messages)
         message = response[0]
         if message.body.startswith("{"):
             item = json.loads(message.body)
@@ -71,4 +73,9 @@ class QueueManager:
         return message, item
 
     def delete(self, message) -> None:
-        self.queue.delete_message(ReceiptHandle=message.receipt_handle)
+        sqs_client.delete_message(
+            QueueUrl=self.queue_url, ReceiptHandle=message.receipt_handle
+        )
+
+    def purge(self) -> None:
+        sqs_client.purge_queue(QueueUrl=self.queue_url)
